528f49348335b0254941078757e9519a0052cb59
Thomas Fisher <tfisher013@gmail.com>
2024-05-05 23:50:28 -0600

    Code cleanup on utils/spectorgrams.py

--- utils/spectrograms.py
+++ utils/spectrograms.py
@@ -6,149 +6,110 @@ import matplotlib.pyplot as plt
 from utils.consts import *
 import shutil
 from PIL import Image
+import sys
 
-def transform_image(path):
-    # Open an image file, ensuring it is read in RGBA mode
+# frame size to use when generating STFTs
+frame_size = 2048
+# hop size to use when generating STFTs
+hop_size = 256
+
+def transform_image(path: str) -> Image:
+    """ Returns an Image object from the path to an image file
+
+        Parameters:
+            path: the path to an image file
+
+        Returns:
+            the Image object corresponding to the provided path
+    """
+
     with open(path, 'rb') as f:
-        img = Image.open(f)
-        return img.convert('RGB')
+        return Image.open(f).convert('RGB')
+    
 
-def generate_spectrograms( path  ):
-    directory = path
-    sub_directories = os.listdir(path)
-    if( '.DS_Store' in sub_directories):
+def generate_spectrograms(dataset_path: str) -> None:
+    """ Function used to create spectrograms using a training dataset of audio files
+
+        Parameters:
+            dataset_path: path to an audio dataset where files are arranged
+                in folders by classes
+    """
+    
+    # perform folder setup
+    sub_directories = os.listdir(dataset_path)
+    if '.DS_Store' in sub_directories:
         sub_directories.remove('.DS_Store')
-    if( os.path.exists(feature_files) != True):
+    if not os.path.exists(feature_files):
          os.mkdir(feature_files)
+
+    # iterate over class folders
     for sub_directory in sub_directories:
         files = []
         
-        if sub_directory.startswith('.DS') != True :
-            files  = os.listdir(os.path.join(directory , sub_directory ))
-        if( os.path.exists(os.path.join(feature_files , sub_directory))):
+        if sub_directory.startswith('.DS') != True:
+            files = os.listdir(os.path.join(dataset_path, sub_directory ))
+        if os.path.exists(os.path.join(feature_files, sub_directory)):
                     shutil.rmtree(os.path.join(feature_files , sub_directory))
-        os.makedirs( feature_files +"/"+ sub_directory)
+        os.makedirs(os.path.join(feature_files, sub_directory))
             
-        print(sub_directory)
-        for file in files:
-            if( file.endswith('.au')):
-                audio, sample_rate = librosa.load(os.path.join(directory , sub_directory , file))
-                stft_audio = librosa.stft(audio, n_fft = frameSize, hop_length = hopSize)
-                # print(stft_audio.shape , 'stft')
+        # iterate over all files per class
+        for filename in files:
+            if filename.endswith('.au'):
+                audio, sample_rate = librosa.load(os.path.join(dataset_path, sub_directory, filename))
+                stft_audio = librosa.stft(audio, n_fft=frame_size, hop_length=hop_size)
                 y_audio = np.abs(stft_audio) ** 2
-                # abs =  plot_spectrogram(y_audio, sample_rate, hopSize , "linear" , file)
-                # abs.savefig( os.path.join( feature_files  , sub_directory ,  f"{file}-abs.png"))
-                # plt.close(abs)
-                y_log_audio = librosa.power_to_db(y_audio , ref = np.max)
-                # print(y_log_audio.shape)
-                
-                amplitude_to_db =  plot_spectrogram(y_log_audio, sample_rate, hopSize, "log" ,file , sub_directory)                
-                
-                plt.close(amplitude_to_db)
+                y_log_audio = librosa.power_to_db(y_audio, ref=np.max)
+                plot_spectrogram(y_log_audio, sample_rate, hop_size, 'log', os.path.join(feature_files, sub_directory, f'{filename}-amp-db.png'))                
 
-def generate_spectrograms_kaggle(path):
-    files = os.listdir(path)
-    directory = kaggle_pred_dir
-    if( os.path.exists(directory) != True):
-        os.mkdir(directory)
-    for file in files:
-            if( file.endswith('.au')):
-                audio, sample_rate = librosa.load(os.path.join(path , file))
-                stft_audio = librosa.stft(audio, n_fft = frameSize, hop_length = hopSize)
-                y_audio = np.abs(stft_audio) ** 2
-                
-                # abs =  plot_spectrogram(y_audio, sample_rate, hopSize , "linear" , file)
-                # abs.savefig( os.path.join( feature_files  , sub_directory ,  f"{file}-abs.png"))
-                # plt.close(abs)
-                y_log_audio = librosa.power_to_db(y_audio , ref = np.max)
-                # print(y_log_audio.shape)
-                amplitude_to_db = plot_spectrogram(y_log_audio, sample_rate, hopSize, "log" ,file)                
-                amplitude_to_db.savefig(os.path.join(kaggle_pred_dir , f"{file}.png"))
-                plt.close(amplitude_to_db)
 
-def generate_spectrograms_validation(path):
-    directory = path
-    sub_directories = os.listdir(path)
-    if( '.DS_Store' in sub_directories):
-        sub_directories.remove('.DS_Store')
-    if( os.path.exists(validation_dir) != True):
-         os.mkdir(validation_dir)
-    for sub_directory in sub_directories:
-        files = []
-        
-        if sub_directory.startswith('.DS') != True :
-            files  = os.listdir(os.path.join(directory , sub_directory ))
-        if( os.path.exists(os.path.join(validation_dir , sub_directory))):
-                    shutil.rmtree(os.path.join(validation_dir , sub_directory))
-        os.makedirs( validation_dir +"/"+ sub_directory)
+def generate_spectrograms_kaggle(path: str) -> None:
+    """ Creates a folder of spectrograms using the provided path to a dataset of test
+            (unlabeled) audio files
+
+        Parameters:
+            path: the path to the test dataset of audio files
+    """
+    
+    if not os.path.exists(kaggle_pred_dir):
+        os.mkdir(kaggle_pred_dir)
+
+    for filename in os.listdir(path):
+        if filename.endswith('.au'):
+            audio, sample_rate = librosa.load(os.path.join(path, filename))
+            stft_audio = librosa.stft(audio, n_fft=frame_size, hop_length=hop_size)
+            y_audio = np.abs(stft_audio) ** 2
             
-        print(sub_directory)
-        for file in files:
-            if( file.endswith('.au')):
-                audio, sample_rate = librosa.load(os.path.join(directory , sub_directory , file))
-                stft_audio = librosa.stft(audio, n_fft = frameSize, hop_length = hopSize)
-                # print(stft_audio.shape , 'stft')
-                y_audio = np.abs(stft_audio) ** 2
-                # abs =  plot_spectrogram(y_audio, sample_rate, hopSize , "linear" , file)
-                # abs.savefig( os.path.join( feature_files  , sub_directory ,  f"{file}-abs.png"))
-                # plt.close(abs)
-                y_log_audio = librosa.power_to_db(y_audio , ref = np.max)
-                # print(y_log_audio.shape)
-                
-                amplitude_to_db =  plot_spectrogram(y_log_audio, sample_rate, hopSize, "log" ,file , sub_directory , mode ="validation")
-                amplitude_to_db.savefig(os.path.join(validation_dir , sub_directory , f"{file}.png"))                
-                plt.close(amplitude_to_db)
+            y_log_audio = librosa.power_to_db(y_audio, ref=np.max)
+            plot_spectrogram(y_log_audio, sample_rate, hop_size, 'log', os.path.join(kaggle_pred_dir, f'{filename}.png'))
 
 
+def plot_spectrogram(y: np.ndarray, sr: int, hop_length: int, y_axis, file_path: str, mode:str='training') -> plt.Figure:
+    """ This function plots a spectrogram using the provided audio samples and 
 
-def plot_spectrogram(y, sr, hop_length, y_axis , file_name   ,sub_directory='' , mode = "training"):
+        Parameters:
+            y: an array of audio samples
+            sr: the sample rate of the audio data
+            hop_length: the hop length used on the audio data
+            y_axis: the y-axis scale, either 'log' or 'linear'
+            file_path: the path to which the generated spectrogram file should be saved
+            mode: 
 
-    print(y.shape)
+    """
 
-    frames = y.shape[1]
-    if( mode == "validation"):
-        figure , axis =  plt.subplots(figsize = (5.12,5.12))
-        image  = librosa.display.specshow(y, sr = sr, hop_length = hop_length ,  x_axis = "time", y_axis = y_axis , cmap='viridis')
-        axis.set_xticks([])
-        axis.set_yticks([])
-        axis.set_xlabel('')
-        axis.set_ylabel('')
-        figure.tight_layout(pad=0)
-        return figure
-    if( sub_directory == ''):
-        figure , axis =  plt.subplots(figsize = (5.12,5.12))
-        image  = librosa.display.specshow(y, sr = sr, hop_length = hop_length ,  x_axis = "time", y_axis = y_axis , cmap='viridis')
-        axis.set_xticks([])
-        axis.set_yticks([])
-        axis.set_xlabel('')
-        axis.set_ylabel('')
-        figure.tight_layout(pad=0)
-        return figure
+    figure, axis = plt.subplots(figsize=(5.12, 5.12))
+    librosa.display.specshow(y, sr=sr, hop_length=hop_length, x_axis='time', y_axis=y_axis, cmap='viridis')
+    axis.set_xticks([])
+    axis.set_yticks([])
+    axis.set_xlabel('')
+    axis.set_ylabel('')
+    figure.tight_layout(pad=0)
+    figure.savefig(file_path)
+    plt.close(figure)
 
-    frames_per_file = frames//3
-    for i in range(3):
-        frame_starting = i*frames_per_file
-        frame_ending = (i+1)*frames_per_file
-        figure , axis =  plt.subplots(figsize = (5.12,5.12))
-        image  = librosa.display.specshow(y[: , frame_starting : frame_ending], sr = sr, hop_length = hop_length ,  x_axis = "time", y_axis = y_axis , cmap='viridis')
-        axis.set_xticks([])
-        axis.set_yticks([])
-        axis.set_xlabel('')
-        axis.set_ylabel('')
-        figure.tight_layout(pad=0)
-        if sub_directory != '':
-            figure.savefig(os.path.join(feature_files ,  sub_directory , f"{file_name}-part-{i}-amp-db.png"))
-            plt.close(figure)
-        else:
-            return figure
-    
-    
-    
-    
-    # figure.colorbar(image , format="%+2.f" , ax = axis )
-    # axis.set_title(f'{file_name}')
-   
 
 if __name__ == "__main__":
+    """ Main method for testing
+    """
+
     generate_spectrograms('data/train/')
 
